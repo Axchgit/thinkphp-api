a90ea888c1868ffb6ef5eab5758b6e25f30f57fb
PC0 <axchtzon@gmail.com>
2020-12-07 14:15:07 +0800

    :sparkles:列出备份文件夹下的文件及其时间大小信息

--- app/controller/DataBack.php
+++ app/controller/DataBack.php
@@ -6,9 +6,10 @@
  * @Author: 罗曼
  * @Date: 2020-12-07 02:56:02
  * @FilePath: \testd:\wamp64\www\thinkphp-api\app\controller\DataBack.php
- * @LastEditTime: 2020-12-07 02:59:15
+ * @LastEditTime: 2020-12-07 14:08:39
  * @LastEditors: 罗曼
  */
+
 namespace app\controller;
 
 // use PHPExcel_IOFactory;
@@ -19,41 +20,147 @@ namespace app\controller;
 
 class DataBack extends Base
 {
+    //查看目录内的文件和目录，并按生成时间排序
+    function getDirContent($file_path = 'E:/backup/')
+    {
+        //要查看的目录
+        // $file_path = '../extend/';
 
-    // public function dataBack()
+        //判断 Mac 是否有 DS_Store，拉取文件是否有.gitkeep、.keep，并排除
+        $files = [];
+        $file = scandir($file_path, 1);
+        if (!empty($file)) {
+            foreach ($file as $k => $v) {
+                if ($v != '.' && $v != '..' && $v != '.DS_Store' && $v != '.gitkeep' && $v != '.keep') {
+                    $files[] = $v;
+                }
+            }
+        }
+
+        $list = [];
+        if (is_array($files)) {
+            foreach ($files as $k => $v) {
+                $filesize = filesize($file_path . $v);
+                if ($filesize < 1024) {
+                    $size = sprintf("%01.2f", $filesize) . "B";
+                } elseif ($filesize < 1024 * 1024) {
+                    $size = sprintf("%01.2f", ($filesize / 1024)) . "KB";
+                } elseif ($filesize < 1024 * 1024 * 1024) {
+                    $size = sprintf("%01.2f", ($filesize / (1024 * 1024))) . "MB";
+                } elseif ($filesize < 1024 * 1024 * 1024 * 1024) {
+                    $size = sprintf("%01.2f", ($filesize / (1024 * 1024 * 1024))) . "GB";
+                }
+                $list[] = [
+                    'file' =>$file_path. $v,
+                    'size' => $size,
+                    'create_time' => filemtime($file_path . $v),
+                    'create_date' => date('Y-m-d H:i:s', filemtime($file_path . $v)),
+                ];
+            }
+        }
+
+        //根据文件和目录生成时间按倒序排列
+        $list = $this->arraySort($list, 'create_time', SORT_DESC);
+        
+        return $this->create($list,'成功');
+
+        // echo '<pre>';
+        // print_r($list);
+        // die;
+        // echo '</pre>';
+    }
+
+    /**
+     * 二维数组根据某个字段排序
+     * @param array $array	要排序的数组
+     * @param string $keys	要排序的键字段
+     * @param string $sort	排序类型: SORT_ASC 升序, SORT_DESC 降序
+     * @return array 		排序后的数组
+     */
+    public function arraySort($array, $keys, $sort = SORT_DESC)
+    {
+        $keysValue = [];
+        foreach ($array as $k => $v) {
+            $keysValue[$k] = $v[$keys];
+        }
+        array_multisort($keysValue, $sort, $array);
+        return $array;
+    }
+
+
+
+    // public function getDirContent($path = 'E:/backup/')
     // {
-    //     $post = request()->param();
-    //     return $this->restoreSql($post['file_name']);
+    //     if (!is_dir($path)) {
+    //         return false;
+    //     }
+    //     //readdir方法
+    //     /* $dir = opendir($path);
+    //     $arr = array();
+    //     while($content = readdir($dir)){
+    //       if($content != '.' && $content != '..'){
+    //         $arr[] = $content;
+    //       }
+    //     }
+    //     closedir($dir); */
+
+    //     //scandir方法
+    //     $arr = array();
+    //     $data = scandir($path);
+    //     foreach ($data as $value) {
+    //         if ($value != '.' && $value != '..') {
+    //             $arr[] = $value;
+    //         }
+    //     }
+    //     return $arr;
     // }
-    public function backupSql($dbname = 'test', $backupFile = false)
+
+
+
+    /**
+     * @description: 数据库备份
+     *@param string $dbname
+
+     *@param bool $backupFile
+
+     *@return {*}
+     */
+    public function backupSql($dbname = 'test', string $backupFile = '+_+_',$mysqldump_path ="D:\/wamp64\/bin\/mysql\/mysql5.7.24\/bin\/")
     {
         // $dbhost = '127.0.0.1';config
         $dbhost = config('database.connections.mysql.hostname');
         $dbuser = config('database.connections.mysql.username');
         $dbpass = config('database.connections.mysql.password');
-        if ($backupFile === false) {
+        if ($backupFile === '+_+_') {
             $backupFile = 'E:/backup/' . $dbname . '_' . date("Y-m-d_His") . '.sql';
         }
         if ($dbpass === '') {
-            exec("D:\wamp64\bin\mysql\mysql5.7.24\bin\mysqldump -h $dbhost -u$dbuser  $dbname > $backupFile");
+            exec($mysqldump_path."mysqldump -h $dbhost -u$dbuser  $dbname > $backupFile");
         } else {
-            exec("D:\wamp64\bin\mysql\mysql5.7.24\bin\mysqldump -h $dbhost -u$dbuser -p$dbpass  $dbname > $backupFile");
+            exec($mysqldump_path."mysqldump -h $dbhost -u$dbuser -p$dbpass  $dbname > $backupFile");
         }
         return  stripslashes($backupFile);
     }
+    /**
+     * @description: 数据库还原
+     *@param string $dbname
 
-    public function restoreSql($backupFile = false, $dbname = 'test')
+     *@param string $backupFile
+
+     *@return {*}
+     */
+    public function restoreSql($backupFile, $dbname = 'test',$mysqldump_path ="D:\/wamp64\/bin\/mysql\/mysql5.7.24\/bin\/")
     {
 
         // $dbhost = '127.0.0.1';config
         $dbhost = config('database.connections.mysql.hostname');
         $dbuser = config('database.connections.mysql.username');
         $dbpass = config('database.connections.mysql.password');
-        $backupFile=stripslashes($backupFile);
+        $backupFile = stripslashes($backupFile);
         if ($dbpass === '') {
-            exec("D:\wamp64\bin\mysql\mysql5.7.24\bin\mysql -h $dbhost -u$dbuser  $dbname < $backupFile");
+            exec($mysqldump_path."mysql -h $dbhost -u$dbuser  $dbname < $backupFile");
         } else {
-            exec("D:\wamp64\bin\mysql\mysql5.7.24\bin\mysql  -h $dbhost -u$dbuser -p$dbpass  $dbname < $backupFile");
+            exec($mysqldump_path."mysql  -h $dbhost -u$dbuser -p$dbpass  $dbname < $backupFile");
         }
         return $backupFile;
     }
